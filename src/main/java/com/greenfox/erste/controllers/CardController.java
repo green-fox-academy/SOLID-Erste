7c679ac271ab0c2771781ebff393e4660f517ce9
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-09-06 00:49:40 +0200

    add(CardController): add POST controller

--- src/main/java/com/greenfox/erste/controllers/CardController.java
+++ src/main/java/com/greenfox/erste/controllers/CardController.java
@@ -1,6 +1,9 @@
 package com.greenfox.erste.controllers;
 
+import com.greenfox.erste.Models.Card;
+import com.greenfox.erste.Models.CardInDTO;
 import com.greenfox.erste.Models.CardValidatorInDTO;
+import com.greenfox.erste.Models.ContactInfo;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +11,7 @@ import com.greenfox.erste.service.ICardService;
 import org.springframework.web.bind.annotation.RequestBody;
 import com.greenfox.erste.service.IContactInfoService;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -22,6 +26,19 @@ public class CardController {
   CardController(ICardService cardService, IContactInfoService contactInfoService) {
     this.cardService = cardService;
     this.contactInfoService = contactInfoService;
+  }
+
+  @PostMapping("ecards")
+  @ResponseBody
+  public ResponseEntity addNewCard(@Valid @RequestBody CardInDTO newCardInDTO) {
+    ContactInfo tempContactInfo = newCardInDTO.getContact();
+    if (!contactInfoService.findAll().contains(tempContactInfo)) {
+      contactInfoService.save(tempContactInfo);
+    }
+    Card cardToAdd = cardService.convertFromDto(newCardInDTO);
+    cardService.save(cardToAdd);
+    return new ResponseEntity(HttpStatus.OK);
+
   }
 
   @PostMapping("/ecards/validate")
