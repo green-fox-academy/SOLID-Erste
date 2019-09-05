066142f29a63cb2ef7cf5e469df356e655ec5356
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-09-05 10:41:22 +0200

    init(proj): create project

---
+++ src/main/java/com/greenfox/erste/ErsteApplication.java
@@ -0,0 +1,14 @@
+package com.greenfox.erste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErsteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErsteApplication.class, args);
+		System.out.println("FUCK");
+	}
+
+}
