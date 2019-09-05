5621260dc9097346cd8cc16368858ae0090db41f
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-09-05 21:21:00 +0200

    add(utils): GlobalDefaultExceptionHandler for all Exceptions

--- src/main/java/com/greenfox/erste/controllers/CardController.java
+++ src/main/java/com/greenfox/erste/controllers/CardController.java
@@ -1,17 +1,8 @@
 package com.greenfox.erste.controllers;
 
-import com.greenfox.erste.Models.Card;
-import com.greenfox.erste.Models.CardInDTO;
-import com.greenfox.erste.Models.ContactInfo;
 import com.greenfox.erste.service.ICardService;
 import com.greenfox.erste.service.IContactInfoService;
-import javax.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -30,17 +21,5 @@ public class CardController {
 
   }
 
-  @PostMapping("ecards")
-  @ResponseBody
-  public ResponseEntity addNewCard(@Valid @RequestBody CardInDTO newCardInDTO) {
-    ContactInfo tempContactInfo = newCardInDTO.getContact();
-    if (!contactInfoService.findAll().contains(tempContactInfo)) {
-      contactInfoService.save(tempContactInfo);
-    }
-    Card cardToAdd = cardService.convertFromDto(newCardInDTO);
-    cardService.save(cardToAdd);
-    return new ResponseEntity(HttpStatus.OK);
-
-  }
 }
 
