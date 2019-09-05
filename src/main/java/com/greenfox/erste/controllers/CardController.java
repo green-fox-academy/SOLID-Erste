e4be5c73b35bce99072c0afa58512e32b3f58731
Iaroslav <iaroslav.miller@gmail.com>
2019-09-05 21:30:09 +0200

    solve db querry

--- src/main/java/com/greenfox/erste/controllers/CardController.java
+++ src/main/java/com/greenfox/erste/controllers/CardController.java
@@ -2,10 +2,14 @@ package com.greenfox.erste.controllers;
 
 import com.greenfox.erste.Models.Card;
 import com.greenfox.erste.service.ICardService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.client.HttpClientErrorException.*;
 
 @RestController
 @RequestMapping("/")
@@ -18,9 +22,12 @@ public class CardController {
   }
 
   @RequestMapping("/ecards/{cardNumber}")
-  public Card getCard(@PathVariable String cardNumber){
+  public ResponseEntity getCard(@PathVariable String cardNumber) throws Exception {
     Card newCard = cardService.findById(cardNumber);
-    return newCard;
+    if (newCard == null){
+     return new ResponseEntity(HttpStatus.NOT_FOUND);
+    }
+    return ResponseEntity.ok(newCard);
   }
 }
 
