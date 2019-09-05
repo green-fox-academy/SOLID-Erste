711edae1235ab684f61c243e7bfb5cca1767a00d
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-09-05 12:18:30 +0200

    add(Controller): add CardController class with constructors

--- src/main/java/com/greenfox/erste/controllers/CardController.java
+++ src/main/java/com/greenfox/erste/controllers/CardController.java
@@ -1,12 +1,6 @@
 package com.greenfox.erste.controllers;
 
-import org.apache.catalina.connector.Response;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,16 +16,5 @@ public class CardController {
 
   }
 
-  @PostMapping("ecards")
-  @ResponseBody
-  public ResponseEntity addCard(/*@RequestBody CardInDTO newCard*/) {
-
-    if (1 == 1) {
-      return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
-    } else {
-      return new ResponseEntity(HttpStatus.OK);
-    }
-
-  }
 }
 
